6b00778d4688c01c9e3ed92a8124149de1e17d46
Mateusz Pusty <mateusz.pusty@panato.game>
2025-04-30 19:57:26 +0200

    Full Flappy Bird logic and UI setup

--- FlappyBird/Assets/BirdController.cs
+++ FlappyBird/Assets/BirdController.cs
@@ -1,16 +1,27 @@
+using System;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BirdController : MonoBehaviour
 {
+    public GameObject gameOverPanel;
+    public TextMeshProUGUI pointsField;
     public Rigidbody2D rigid;
     public float jumpForce;
 
-    private bool hasStarted;
+    public static bool hasStarted;
+    public static bool gameOver;
+    
     private float gravityScale;
+    private int points;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        hasStarted = false;
+        gameOver = false;
+        
         gravityScale = rigid.gravityScale;
         rigid.gravityScale = 0;
     }
@@ -18,6 +29,9 @@ public class BirdController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(gameOver == true)
+            return;
+        
         if (Input.GetButtonDown("Jump"))
         {
             if(hasStarted == false)
@@ -29,5 +43,25 @@ public class BirdController : MonoBehaviour
             rigid.linearVelocity = Vector2.zero;
             rigid.AddForceY(jumpForce, ForceMode2D.Impulse);
         }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        gameOver = true;
+        gameOverPanel.SetActive(true);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Points"))
+        {
+            points++;
+            pointsField.text = points.ToString();
+        }
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene("FlappyBird");
     }
 }
--- FlappyBird/Assets/MoveController.cs
+++ FlappyBird/Assets/MoveController.cs
@@ -6,21 +6,36 @@ public class MoveController : MonoBehaviour
     public float spawnPositionX;
     public float despawnPositionX;
 
+    public bool shouldRandomizeY;
+    public float minY;
+    public float maxY;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        if (shouldRandomizeY)
+        {
+            transform.position = new Vector3(transform.position.x, Random.Range(minY, maxY), transform.position.z);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(BirdController.hasStarted == false || BirdController.gameOver == true)
+            return;
+        
         transform.position = new Vector3(transform.position.x + speed * Time.deltaTime,
             transform.position.y, transform.position.z);
 
         if(transform.position.x <= despawnPositionX)
         {
             transform.position = new Vector3(spawnPositionX, transform.position.y, transform.position.z);
+
+            if (shouldRandomizeY)
+            {
+                transform.position = new Vector3(transform.position.x, Random.Range(minY, maxY), transform.position.z);
+            }
         }
     }
 }
